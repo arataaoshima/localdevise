6414f79b3e4acc6ae955940a35869578819aba12
jruby -S gem install bundler <newbluelight1031@gmail.com>
2019-09-24 20:48:42 -0700

    no need password to update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    omniauth_callbacks: "users/omniauth_callbacks"
+    omniauth_callbacks: "users/omniauth_callbacks",
+    registrations: 'registrations'
   }
   root to: 'home#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
